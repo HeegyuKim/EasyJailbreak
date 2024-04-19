08aab5bf0837cf18254077923394b5969f65cbf6
HeegyuKim <heekue83@gmail.com>
2024-04-19 05:55:33 +0000

    fix recursion error

--- script/serve/starling-lm-7b-alpha.sh
+++ script/serve/starling-lm-7b-alpha.sh
@@ -1,4 +1,5 @@
 python -m experiments.run_server \
     --model_name berkeley-nest/Starling-LM-7B-alpha \
     --prompt_length 1024 \
-    --max_new_tokens 1024 
+    --max_new_tokens 1024 \
+    --fully_sharded_data_parallel False 
